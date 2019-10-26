189623a3d46c9d69ebb62e689d871759705de795
ilyas özkaya <appinventer.ilyas@gmail.com>
2019-10-26 21:41:52 +0300

    g

--- MyApi/Controllers/AdminController.cs
+++ MyApi/Controllers/AdminController.cs
@@ -33,7 +33,7 @@ namespace MyApi.Controllers
 
     }
     //public IActionResult Index()
-    //{////// safasfasfafdsdfsdf
+    //{////// safasfasfafdsdfsdf/////
     //    return View();
     //}///////
     [Route("ConfirmUser")]
